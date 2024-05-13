0d5dade2ce2d3974d05617881aacc1f07d53c010
박상욱 <vnlfltmxm@gmail.com>
2024-05-13 15:55:36 +0900

    푸쉬용

--- Assets/Turret/Scripts/MissleTurret.cs
+++ Assets/Turret/Scripts/MissleTurret.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class MissleTurret : Turret
 {
-    // Start is called before the first frame update
-    void Start()
+    private int nowUpgradeCount = 0;
+    private int nowHp;
+    private int maxHp = 5;
+    private int hpRise = 1;
+    private float makingTime = 15;
+    private float attackDamge = 100;
+    private float attackSpeed = 0.35f;
+    private float attackRange = 20;
+    private float upgradeTime = 15;
+    private float repairTime = 15;
+    private float attackRise = 50;
+    private float attackSpeedRise = 0.2f;
+    private float upgradCostRise = 1.5f;
+    private float maxUpgradeCount = 3;
+    private float repairCost = 10;
+    private float upgradeCost = 20;
+    private float makingCost = 30;
+
+    private void Awake()
     {
-        
+        SetTurret(makingTime, makingCost, attackDamge, attackSpeed, attackRange, maxHp, hpRise, upgradeCost, upgradeTime, repairTime, repairCost, attackRise, attackSpeedRise, upgradCostRise, maxUpgradeCount);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Attack()
     {
-        
+       
     }
+
 }
